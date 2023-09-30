38504cf815d7fcedbe2f8d07ff8296fb016eb173
Shuffled72 <shuffled720@gmail.com>
2023-09-30 13:04:28 +0530

    today-done

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,17 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+
+
 export const metadata = {
   title: 'EE Time Table',
   description: 'Electrical is very important',
-  
+
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body class={inter.class}>{children}</body>
+      <body >{children}</body>
     </html>
   )
 }
