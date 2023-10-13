431b877d547cfde913bfee8ba1d433f9908baa33
Shuffled72 <shuffled720@gmail.com>
2023-10-13 09:45:15 +0530

    link added

--- app/page.js
+++ app/page.js
@@ -19,7 +19,10 @@ export default function Home() {
 
   return (
     <div className="h-screen bg-red-50">
-      <h1 className="text-center text-3xl py-5">Electrical Time Table</h1>
+      <a style={{ textDecoration: "none" }} target="_blank" href="https://docs.google.com/spreadsheets/d/1bOobFVUvOgPX-3OM-u3mBGratwJLuMQis591XnCb5nQ/edit?usp=drivesdk">
+
+        <h1 className="text-center text-3xl py-5">Electrical Time Table</h1>
+      </a>
       <hr />
       <section id="todays">
         <div className="px-4">
